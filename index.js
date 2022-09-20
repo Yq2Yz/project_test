3bf0f36bb738d119147bccc8674a1fa09a2113dd
Yq2Yz <2889816679@qq.com>
2022-09-21 04:05:38 +0800

    第一次提交到Git仓库

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    // 1. 通过 DOM 操作，获取 btn 的元素
+    var btn = document.querySelector('#btn');
+
+    // 2. 监听 元素的 submit 事件
+    btn.addEventListener('click', function() {
+        console.log('创建一个Git仓库');
+        alert('创建一个Git仓库')
+    })
+}
