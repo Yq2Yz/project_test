c0dbeda8a038398bfb65489033c6553e4ea06b8c
Yq2Yz <2889816679@qq.com>
2022-09-22 19:06:54 +0800

    修改了index页面的内容

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ window.onload = function() {
     btn.addEventListener('click', function() {
         console.log('创建一个Git仓库');
         alert('创建一个Git仓库')
-		console.log('修改了内容')
+        console.log('修改了内容')
     })
+}
+
+function demoFunction() {
+    var x;
+    var person = prompt("请输入您的名字", "哈利波特");
+    if (person != null && person != "") {
+        x = "您好" + person + "! 今天感觉如何？";
+        document.getElementById("demo").innerHTML = x;
+    }
 }
